445f7ee657978e54d78c14e19698025c4096fe6c
Tobias Christiansen <tobi499a@mail.hansenberg.dk>
2022-08-16 12:50:14 +0200

    Adventure Story
    Ved skillevejen er det kun muligt at gå til højre

--- 1_Basics/Program.cs
+++ 1_Basics/Program.cs
@@ -31,20 +31,93 @@ comment
 #endregion
 
 #region Variables
-Console.WriteLine("What is your name?");
+Console.WriteLine("Hello adventurer! \nWould you like to hear a story?");
 
-//<Type> <Name> = <Value> ;
-string myString = "This is how to define a string variable!";
+string answer = Console.ReadLine();
+bool StartStory = false;
+if (answer == "yes")
+{
+    StartStory = true;
+}
 
-//Reading a string from the console
-myString = Console.ReadLine();
+if (StartStory == true)
+{
+    Console.WriteLine("Okay firstly I need your name");
 
-//Writing it back to the console
-Console.WriteLine("Aaah, welcome " + myString + ", nice to meet you!");
+    string AName = Console.ReadLine();
+    Console.WriteLine("\nOkay " + AName + "! \nThe story starts deep in a forest, far away from nearest civilization.\n");
+    Console.WriteLine("You are running in a direct direction but suddenly a t-shaped path forms. Which way do you run?");
+    Console.WriteLine("\nLeft\nRight\nStraight\n");
+
+    answer = Console.ReadLine();
+    //Start the storyline of turning right
+    if (answer == "right")
+    {
+        Console.WriteLine("\nYou go Right, and after you run for almost 5 minutes you see a scary looking house.\nDo you dare to enter?");
+        answer = Console.ReadLine();
+
+        //Enter the house
+        if (answer == "yes")
+        {
+            Console.WriteLine("You hurry to the house and knock");
+            Thread.Sleep(1200);
+            Console.WriteLine("*knock* *knock*");
+            Thread.Sleep(1200);
+            Console.WriteLine("Silence");
+            Thread.Sleep(1200);
+            Console.WriteLine("You slowly push the door open and hear a creaking sound.");
+            Console.WriteLine("Cockroaches run all over the floor, making it so that every step from now on will make a sound.");
+            Console.ReadLine();
+        }
+
+        //Run past the house
+        else if (answer == "no")
+        {
+            Console.WriteLine("\nYou run past the spooky looking house, and until you enter a large grassfield.\nIt seems like you have escaped the cursed forrest.");
+            Console.WriteLine("Some hours pass by and you find you'r village and house.\nYou can finally rest");
+            Console.ReadLine();
+        }   
+    }
+
+    //Start the storyline of turning left
+    else if (answer == "left")
+    {
+
+
+        Console.ReadLine();
+
+    }
+
+    //Start the storyline of going straight
+    else if (answer == "straight")
+    {
+
+
+        Console.ReadLine();
+    }
+
+}
+
+else if (answer == "no")
+{
+    Console.WriteLine("Im sorry to hear that");
+    Console.ReadLine(); //Stops program from exiting until we press a key
+}
+
+
 #endregion
 
-#region Data types
-Console.WriteLine("Do you want to learn about the simplest data types?");
+
+/* Teachers corner
+
+* Operators; +, -, /, *, %, ++, --, *=, +=, -=
+
+* Casting
+int.Parse()
+Convert.ToInt32()
+(int)
+*/
+
 /*
 * The four main basic types we we will work with in the beginning are:
 * bool, Int, Float and String   
@@ -58,64 +131,4 @@ Console.WriteLine("Do you want to learn about the simplest data types?");
 *
 * There are many different data types which can be explored in the Microsoft C# documentation, see link below.
 * https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/builtin-types/built-in-types
-*/
-
-string answer = Console.ReadLine();
-
-bool LearnAboutDataTypes = false;
-if(answer == "yes"){
-    LearnAboutDataTypes = true;
-}
-
-if(LearnAboutDataTypes == true){
-    Console.WriteLine("Then you have come to the right place!");
-    Console.WriteLine("The 'bool' type can either be 'true' or 'false', nothing more, nothing less.");
-    Console.WriteLine("When you answered 'yes', a bool named 'LearnAboutDataTypes' was set to 'true'");
-    Console.WriteLine("And allowed for these messages!");
-    Console.WriteLine("...");
-    Console.ReadLine();
-    
-    Console.WriteLine("Now... The next data type we are gonna talk about is the integer, called an 'int'");
-    Console.WriteLine("It is used to store whole numbers, like 1,2,3,4 and so on.");
-    Console.WriteLine("Try inserting an integer now:");
-    string inputFromConsole = Console.ReadLine();
-    int number = int.Parse(inputFromConsole);
-    
-    Console.WriteLine("I have now converted a text string into an integer!");
-    number = number + 1;
-    //number++;
-    //number += 1;
-    Console.WriteLine("We can now do math operations on it, and voila, your number + 1 is equal to = " + number);
-    Console.WriteLine("...");
-    Console.ReadLine();
-
-    Console.WriteLine("Next up we have the Float data type");
-    Console.WriteLine("Float is decimal numbers, you can do math on it, just as we did with the integer");
-    Console.WriteLine("Remember to use 'f'!");
-    float OneThird = 1.0f/3.0f; //Notice the 'f' to indicate this number is a float value
-    Console.WriteLine("Like for example 1.0f/3.0f, which gives : " + OneThird);
-    Console.WriteLine("where as 1/3 gives = " + 1/3 + ". Because the computer thinks it is working with whole numbers (integers)");
-    Console.WriteLine("...");
-    Console.ReadLine();
-
-    Console.WriteLine("Lastly, the string data type.");
-    Console.WriteLine("Strings are text, and can be found between quotation marks");
-    Console.WriteLine("We have used strings to read and write to the console, this line is a string");
-    Console.WriteLine("...");
-    Console.ReadLine();
-}
-Console.WriteLine("...The end...");
-Console.ReadLine(); //Stops program from exiting until we press a key
-
-#endregion
-
-
-/* Teachers corner
-
-* Operators; +, -, /, *, %, ++, --, *=, +=, -=
-
-* Casting
-int.Parse()
-Convert.ToInt32()
-(int)
 */
