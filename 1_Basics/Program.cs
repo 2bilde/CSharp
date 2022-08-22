15a2169db7b20664bd639fd2a6d31d820c3f808e
Tobias Christiansen <tobi499a@mail.hansenberg.dk>
2022-08-22 12:50:00 +0200

    Randomizer for fight

--- 1_Basics/Program.cs
+++ 1_Basics/Program.cs
@@ -1,4 +1,6 @@
-﻿#region 
+﻿
+
+#region 
 //The "#region" can be used to 'collapse' areas of code so you can focus on the key areas
 //Comments can be done in a couple different ways, after // and multiline between /* comment */
 /*
@@ -66,17 +68,162 @@ if (StartStory == true)
             Console.WriteLine("Silence");
             Thread.Sleep(1200);
             Console.WriteLine("You slowly push the door open and hear a creaking sound.");
+            Thread.Sleep(1000);
             Console.WriteLine("Cockroaches run all over the floor, making it so that every step from now on will make a sound.");
-            Console.ReadLine();
+            Thread.Sleep(3000);
+            Console.WriteLine("\nAs you enter the room and kill the first 3 cockroaches, the door slams behind you");
+            Thread.Sleep(1000);
+            Console.WriteLine("The silence becomes loud and as your eyes adapt to the dark room \nYou hear a sudden laugh from what feels like under you");
+            Thread.Sleep(6000);
+            Console.WriteLine("\nThe room you are standing in seems like a corridor with 3 doors.\nOne on the right and 2 on the left.");
+            Thread.Sleep(2000);
+            Console.WriteLine("The door on the left is wide open and you spot what looks like a fridge and an oven.");
+            Thread.Sleep(1500);
+            Console.WriteLine("The wierd sound seems to be comming from the first door on the right.");
+            Thread.Sleep(2000);
+
+            Console.WriteLine("\nleave\ndoor on left\n1 door right\n2 door right\n");
+
+            answer = Console.ReadLine();
+
+            switch (answer)
+            {
+                //Leave the house
+                case "leave":
+                    {
+                        Console.WriteLine("You quickly turn around and try to open the door, making a loud sound, but the door wont open.");
+                        Thread.Sleep(2500);
+                        Console.WriteLine("Behind you, you hear a very loud step. You turn around and before you realize whats happening");
+                        Thread.Sleep(2000);
+                        Console.WriteLine("A claw hits across your face turning everything dark");
+                        Thread.Sleep(700);
+                        Console.WriteLine(".");
+                        Thread.Sleep(700);
+                        Console.WriteLine(".");
+                        Thread.Sleep(700);
+                        Console.WriteLine(".");
+                        Console.ReadLine();
+                        break;
+                    }
+                case "door on left":
+                    {
+                        //Turn left to kitchen
+                        Console.WriteLine("You quickly run to the kitchen, and it seems like the steps under you become louder.");
+                        Thread.Sleep(2000);
+                        Console.WriteLine("Pots and pans are on the stove and some knives hang on the wall, the fridge is filled with food.");
+                        Thread.Sleep(2000);
+                        Console.WriteLine("Growling apears behind you from what seems like the first door. You cant spot it yet.");
+                        Thread.Sleep(2000);
+                        Console.WriteLine("There doesnt seem to be any hiding spaces other than the window that is wide open.");
+                        Thread.Sleep(2000);
+                        Console.WriteLine("\nFlee\nFight\n");
+                        answer = Console.ReadLine();
+
+                        //Fight
+                        if (answer == "fight")
+                        {
+                            Random randomNumbers = new Random(); // random number generator for weapon
+                            int Weapon = randomNumbers.Next(1, 3);
+
+                            switch (Weapon)
+                            {
+                                case 1:
+                                    //Knife
+                                    Console.WriteLine("You reach for the biggest knife in the knifeblock and pull it out");
+                                    Thread.Sleep(1200);
+                                    Console.WriteLine("just to find out that it was the smallest knife you got");
+                                    Thread.Sleep(1200);
+                                    Console.WriteLine("After grabbing the knife it seems near to impossible to kill the growling thing");
+                                    Thread.Sleep(1400);
+                                    Console.WriteLine("You run towards the sound and stab around the corner into what now seems like a big scream");
+                                    Thread.Sleep(1600);
+                                    Console.WriteLine("You look down and see that you have stabbed a small girl in the neck");
+                                    Thread.Sleep(1200);
+                                    Console.WriteLine("She falls to the ground and behind her a big dark creature apears in the shadow");
+                                    Thread.Sleep(1500);
+                                    Console.WriteLine("Out of guilt and fear you wank the knife towards yourself and everything becomes dark");
+                                    Thread.Sleep(700);
+                                    Console.WriteLine(".");
+                                    Thread.Sleep(700);
+                                    Console.WriteLine(".");
+                                    Thread.Sleep(700);
+                                    Console.WriteLine(".");
+                                    Console.ReadLine();
+                                    break;
+                                case 2:
+                                    Console.WriteLine("You panic and freeze. Your mind wants to grab the nearest pan but cant");
+                                    Thread.Sleep(1600);
+                                    Console.WriteLine("Something big enters the kitchen and growls at you.");
+                                    Thread.Sleep(1000);
+                                    Console.WriteLine("You cant seem to put a finger on what it is but it doesnt matter");
+                                    Thread.Sleep(1100);
+                                    Console.WriteLine("It lunches at you and everything becomes dark");
+                                    Thread.Sleep(700);
+                                    Console.WriteLine(".");
+                                    Thread.Sleep(700);
+                                    Console.WriteLine(".");
+                                    Thread.Sleep(700);
+                                    Console.WriteLine(".");
+                                    Console.ReadLine();
+                                    break;
+                                case 3:
+                                    Console.WriteLine("You panic and freeze. Your mind wants to grab the nearest pan but cant");
+                                    Thread.Sleep(1600);
+                                    Console.WriteLine("A big shadow apears on the wall due to the kitchen light");
+                                    Thread.Sleep(1200);
+                                    Console.WriteLine("The shadow becomes bigger and bigger and as it turns the corner");
+                                    Thread.Sleep(400);
+                                    Console.WriteLine(".");
+                                    Thread.Sleep(400);
+                                    Console.WriteLine(".");
+                                    Thread.Sleep(400);
+                                    Console.WriteLine(".");
+                                    Thread.Sleep(1000);
+                                    Console.WriteLine("\nA small hamster looking creature on its back legs apears");
+                                    Thread.Sleep(1200);
+                                    Console.WriteLine("It looks like it has rabies. You run towards it and kick it");
+                                    Thread.Sleep(1200);
+                                    Console.WriteLine("You grab the pan and smacks it till it die\n");
+                                    Thread.Sleep(1200);
+                                    Console.WriteLine("You find a bed after its dead and the next morning you head to your village and find your home");
+                                    Thread.Sleep(1200);
+                                    Console.WriteLine("The same day you quit your job as an adventure. You have had too much\n");
+                                    Thread.Sleep(1200);
+                                    Console.WriteLine("Thanks for listening to my adventure brave one :D");
+                                    Console.WriteLine();
+                                    break;
+                            }
+
+
+                        }
+
+                        //Flee
+
+                        break;
+                    }
+
+
+                    //Go to the noice
+
+
+                    //Far door
+
+
+                    Console.ReadLine();
+            }
+
+
+
         }
 
         //Run past the house
         else if (answer == "no")
         {
             Console.WriteLine("\nYou run past the spooky looking house, and until you enter a large grassfield.\nIt seems like you have escaped the cursed forrest.");
+            Thread.Sleep(500);
             Console.WriteLine("Some hours pass by and you find you'r village and house.\nYou can finally rest");
             Console.ReadLine();
-        }   
+        }
     }
 
     //Start the storyline of turning left
